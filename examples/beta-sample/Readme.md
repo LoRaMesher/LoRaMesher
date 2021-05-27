c6775d9e0cee9cbb3f7f3731453d179a13cf9b50
Sergi Miralles <uxx4pv85@mailer.me>
2021-05-27 20:47:07 +0200

    Reestructures project and adds example

---
+++ examples/beta-sample/Readme.md
@@ -0,0 +1,1 @@
+This is a very simple example to check that the library actually does something. Don't expect anything useful out of this code
---
+++ examples/beta-sample/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include "loramesher.h"
+
+
+LoraMesher *radio;
+
+
+void setup() {
+  Serial.begin(9600);
+  radio = new LoraMesher();
+}
+
+void loop() {
+  sleep(7);
+  radio->sendDataPacket();
+}
