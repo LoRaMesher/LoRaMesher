45617a86cd7951b3bcfa0b9726dd54774074cebc
Jaimi5 <janms2009@gmail.com>
2022-01-07 23:11:29 +0100

    LoRaMesher- Main fix

--- examples/beta-sample/src/main.cpp
+++ examples/beta-sample/src/main.cpp
@@ -98,14 +98,12 @@ void setup() {
 
 
 void loop() {
-    for (;;) {
-        // Log.trace(F("Send packet %d" CR), dataCounter);
-        helloPacket->counter = dataCounter++;
+    Log.trace(F("Send packet %d" CR), dataCounter);
+    helloPacket->counter = dataCounter++;
 
-        //Create packet and send it.
-        radio->createPacketAndSend(BROADCAST_ADDR, helloPacket, 1);
+    //Create packet and send it.
+    radio->createPacketAndSend(BROADCAST_ADDR, helloPacket, 1);
 
-        //Wait 20 seconds to send the next packet
-        vTaskDelay(20000 / portTICK_PERIOD_MS);
-    }
+    //Wait 20 seconds to send the next packet
+    vTaskDelay(20000 / portTICK_PERIOD_MS);
 }
