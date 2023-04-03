84ff0ae285ba4dcfc570770aaa94f21412a47068
Jaimi5 <janms2009@gmail.com>
2023-04-03 16:44:10 +0200

    Removed previous wifi functions

--- src/services/MACService.cpp
+++ src/services/MACService.cpp
@@ -1,13 +1,6 @@
 #include "MACService.h"
 
 void MACService::init() {
-    // uint8_t WiFiMAC[6];
-
-    // WiFi.macAddress(WiFiMAC);
-    // localAddress = (WiFiMAC[4] << 8) | WiFiMAC[5];
-
-    // Log.noticeln(F("Local LoRa address (from WiFi MAC): %X"), localAddress);
-
     uint8_t _chipmacid[6];
 
     esp_efuse_mac_get_default(_chipmacid);
