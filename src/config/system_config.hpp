3d2e9b19d07d0ad94fa5e3385bed8c283f538c94
Jaimi5 <janms2009@gmail.com>
2025-05-02 15:51:07 +0200

    LoRaMesher - Remove ESP_LOGI macro and set log level to DEBUG in system configuration

--- src/config/system_config.hpp
+++ src/config/system_config.hpp
@@ -10,11 +10,10 @@
 // Native implementation
 #include <stdio.h>
 #define LORAMESHER_BUILD_NATIVE
-#define ESP_LOGI(tag, fmt, ...) printf(fmt "\n", ##__VA_ARGS__)
 #endif
 
 // #define DEBUG  // Uncomment to enable debug functionality
 
-#define LORAMESHER_LOG_LEVEL 4  // 0=DEBUG, 1=INFO, 2=WARNING, 3=ERROR, 4=NO_LOG
+#define LORAMESHER_LOG_LEVEL 0  // 0=DEBUG, 1=INFO, 2=WARNING, 3=ERROR, 4=NO_LOG
 //#define LOGGER_DISABLE_COLORS   // Disable color output
 #define LOGGER_BUFFER_SIZE 128  // Adjust buffer size for your needs
